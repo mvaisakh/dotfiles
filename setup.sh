f512e5fb143834e912dd4e76ede0c603f904090a
merothh <merothh@gmail.com>
2020-10-16 14:37:47 +0530

    $PATH: modify from .profile instead

--- setup.sh
+++ setup.sh
@@ -41,7 +41,7 @@ case $release in
 	;;
 esac
 
-backup_list=(.p10k.zsh .vimrc .Xresources .zshrc)
+backup_list=(.p10k.zsh .profile .vimrc .Xresources .zshrc)
 symlink_list=(.config/i3 .config/git .config/polybar .config/rofi .fonts/Material-Icons .fonts/MesloLGS-NF .p10k.zsh .vimrc .Xresources)
 dir_list=(.fonts .config Pictures/Screenshots)
 
@@ -83,5 +83,5 @@ done
 echo -e "\n$($cyan)// All done. Make sure to:\n
   1. Set themes and fonts using $($yellow)lxappearance $($cyan)after logging into dwm\n
   2. Log out and back in for $($yellow)zsh $($cyan)to kick in\n
-  3. Your previous $($yellow).bashrc .zshrc .Xresources $($cyan) are at $($yellow) ~/dotfiles/.backup $($cyan)\n
+  3. Some general configs have been backed to $($yellow) ~/dotfiles/.backup $($cyan)\n
   4. Maybe move useful code from previous $($yellow).zshrc $($cyan)or $($yellow).bashrc\n $($reset)"
