9972b1624c346619311fd9bdf75ac75d0b2bb31d
Luke Smith <luke@lukesmith.xyz>
2021-04-10 13:09:57 +0530

    setup.sh: enable tap to click by default
    Apparently my left mouse button is broken so I really need this.
    
    Signed-off-by: Daniel Jacob Chittoor <djchittoor47@gmail.com>

--- setup.sh
+++ setup.sh
@@ -68,6 +68,16 @@ fi
 sudo rmmod pcspkr snd_pcsp &>/dev/null
 printf "blacklist pcspkr\nblacklist snd_pcsp" | sudo tee /etc/modprobe.d/nobeep.conf 1>/dev/null
 
+# tap to click
+[ ! -f /etc/X11/xorg.conf.d/40-libinput.conf ] && printf 'Section "InputClass"
+        Identifier "libinput touchpad catchall"
+        MatchIsTouchpad "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "libinput"
+    # Enable left mouse button by tapping
+    Option "Tapping" "on"
+EndSection' > /etc/X11/xorg.conf.d/40-libinput.conf
+
 # backup some specified files
 rm -rf ~/dotfiles/.backup
 mkdir ~/dotfiles/.backup
