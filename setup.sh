f2e46cb76346b395ea9feb259b2a5c0d30f7fb48
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2021-04-11 12:35:40 +0530

    .local/bin: copy screenshot to clipboard
    Reproduce the same behaviour of screenshots in windows.
    Install and use xclip instead of xsel, the default X selection
    tool as it can not extract binary data from clipboard.
    
    Signed-off-by: Daniel Jacob Chittoor <djchittoor47@gmail.com>

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ read -r yay
 case $release in
 	"Arch Linux" | "Artix Linux" | "Manjaro Linux")
 
-		packages="adwaita-icon-theme feh gnome-themes-extra lxappearance otf-font-awesome papirus-icon-theme picom playerctl pulseaudio rxvt-unicode scrot urxvt-perls xorg-xbacklight dmenu"
+		packages="adwaita-icon-theme feh gnome-themes-extra lxappearance otf-font-awesome papirus-icon-theme picom playerctl pulseaudio rxvt-unicode scrot urxvt-perls xorg-xbacklight dmenu xclip"
 
 		[ ! "$zsh" = "y" ] || packages+=" zsh zsh-theme-powerlevel10k"
 
