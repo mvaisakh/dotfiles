mvaisakh/dotfiles

commit 21e72337dca479001187ba6cace710d589a9f1d3

setup.sh: create empty config file for wget
As per XDG directory specification we force wget to have its config file in
~/.config rather than in ~/ but wget kind of errors out as it is unable to
find wgetrc therefore touch a empty config to satisfy wget.

Signed-off-by: Daniel Jacob Chittoor <djchittoor47@gmail.com>

setup.sh
@@ -62,7 +62,7 @@ esac
 
 backup_list=(.vimrc .Xresources .zshenv .zshrc)
 symlink_list=(.config/git .config/zsh .local/bin .local/share/dwm .local/share/fonts .profile .vimrc .Xresources .zshenv)
-dir_list=(.config .cache/zsh .local/share Pictures/Screenshots)
+dir_list=(.config .config/wget/  .cache/zsh .local/share Pictures/Screenshots)
 
 if [ "$zsh" = "y" ]; then
 	symlink_list+=(" .zshrc")
@@ -100,6 +100,9 @@ done
 for dir in ${dir_list[*]}; do
 	mkdir -p ~/"$dir"
 done
+
+# create empty config files for programs
+touch .config/wget/wgetrc
 
 # go ahead and symlink everything
 for file in ${symlink_list[*]}; do

commit f639056fb03cbb60c53dc98ad34d1c3935e7e578

setup.sh: add home shorthand to touch command
Fixes the error out introduced in 21e72337dca479001187ba6cace710d589a9f1d3

Signed-off-by: Daniel Jacob Chittoor <djchittoor47@gmail.com>

setup.sh
@@ -102,7 +102,7 @@ for dir in ${dir_list[*]}; do
 done
 
 # create empty config files for programs
-touch .config/wget/wgetrc
+touch ~/.config/wget/wgetrc
 
 # go ahead and symlink everything
 for file in ${symlink_list[*]}; do